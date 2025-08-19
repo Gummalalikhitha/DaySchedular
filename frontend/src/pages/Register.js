2ea369e9ee3f320cc989ff2ea4b8344486bd299a
Gummala Likhitha <157954083+Gummalalikhitha@users.noreply.github.com>
2025-08-19 15:14:37 +0530

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -21,7 +21,7 @@ const Register = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", {
+      const res = await axios.post("https://dayschedular-backend.onrender.com/api/auth/register", {
         username,
         email,
         password,
