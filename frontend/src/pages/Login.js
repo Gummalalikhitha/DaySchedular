c815d6ad719d4581e3f97b84ba84f3961c5355e4
Gummala Likhitha <157954083+Gummalalikhitha@users.noreply.github.com>
2025-08-19 15:13:29 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://dayschedular-backend.onrender.com/api/auth/login", {
         username,
         password,
       });
