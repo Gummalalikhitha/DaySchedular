9d6bc967403c5c2f3424ea79ddf9520ec97a26b5
Gummala Likhitha <157954083+Gummalalikhitha@users.noreply.github.com>
2025-08-19 15:15:13 +0530

    Update ResetPassword.js

--- frontend/src/pages/ResetPassword.js
+++ frontend/src/pages/ResetPassword.js
@@ -15,7 +15,7 @@ const ResetPassword = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/reset-password", {
+      const res = await axios.post("https://dayschedular-backend.onrender.com/api/auth/reset-password", {
         email,
         newPassword,
         confirmPassword,
